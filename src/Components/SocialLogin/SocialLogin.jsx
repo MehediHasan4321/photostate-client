5e8cef9ebb21c510ed118d6909ba169a7ba95441
MehediHasan4321 <126mehedi@gmail.com>
2023-06-08 09:01:20 +0600

    social loging successfully compelte

--- src/Components/SocialLogin/SocialLogin.jsx
+++ src/Components/SocialLogin/SocialLogin.jsx
@@ -1,7 +1,25 @@
+import { useLocation, useNavigate } from "react-router-dom";
+import { useAuth } from "../../Utlites/useAuth";
+import { toast } from "react-hot-toast";
+import { saveUserToDB } from "../../AllApi/saveUserToDB";
 
 const SocialLogin = () => {
+    const {loginWithGoogle} = useAuth()
+    const location = useLocation()
+    const navigate = useNavigate()
+    const from = location?.state?.from?.pathname || '/'
     const handleSocialLogin = ()=>{
-
+        loginWithGoogle()
+        .then(result=>{
+            saveUserToDB(result.user)
+            .then(data=>{
+                if (data.upsertedId || data.modifiedCount > 0) {
+                    toast.success('Login With Google Successfully')
+                    navigate(from, { replace: true })
+                }
+            })
+        })
+        .catch(err=>toast.error(err.message))
     }
     return (
         <div className='mt-5'>
